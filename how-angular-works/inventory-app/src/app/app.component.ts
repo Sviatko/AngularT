5c7bb09750c18fc324fedd194361e6d3bce6d4f2
Sviatoslab Dubyniak <sdubyn@softserveinc.com>
2017-07-25 15:40:26 +0300

    iventory

--- how-angular-works/inventory-app/src/app/app.component.ts
+++ how-angular-works/inventory-app/src/app/app.component.ts
@@ -1,9 +1,6 @@
-import {
-  Component,
-  EventEmitter
-} from '@angular/core';
-
-import { Product } from './product.model';
+import {Component, EventEmitter} from '@angular/core';
+import {Product} from './product.model';
+import {ProductService} from './product-service.service';
 
 /**
  * @InventoryApp: the top-level component for our application
@@ -14,31 +11,27 @@ import { Product } from './product.model';
 })
 export class AppComponent {
   products: Product[];
+  searchStr:string = '';
+  product:Product = new Product('','','',[''],0);
+  departments:string;
 
-  constructor() {
-    this.products = [
-      new Product(
-        'MYSHOES',
-        'Black Running Shoes',
-        '/assets/images/products/black-shoes.jpg',
-        ['Men', 'Shoes', 'Running Shoes'],
-        109.99),
-      new Product(
-        'NEATOJACKET',
-        'Blue Jacket',
-        '/assets/images/products/blue-jacket.jpg',
-        ['Women', 'Apparel', 'Jackets & Vests'],
-        238.99),
-      new Product(
-        'NICEHAT',
-        'A Nice Black Hat',
-        '/assets/images/products/black-hat.jpg',
-        ['Men', 'Accessories', 'Hats'],
-        29.99)
-      ];
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getProducts();
+  }
+
+  addProduct(): boolean {
+    // console.log(`Adding article title: ${title.value} , sku:${sku.value} and link: ${image.value}`);
+    // let product:Product = new Product( sku.value, title.value, image.value, department.value.split(' '),price.value);
+    this.product.department = this.departments.split(' ');
+    this.productService.addProduct(this.product);
+    return false;
   }
 
   productWasSelected(product: Product): void {
     console.log('Product clicked: ', product);
+  }
+
+  getProduct(){
+    return this.products.filter(product => product.name.toLowerCase().includes(this.searchStr.toLowerCase()))
   }
 }
--- how-angular-works/inventory-app/src/app/app.module.ts
+++ how-angular-works/inventory-app/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {LocalStorageModule} from 'angular-2-local-storage';
 
-import { AppComponent } from './app.component';
-import { ProductImageComponent } from './product-image/product-image.component';
-import { ProductDepartmentComponent } from './product-department/product-department.component';
-import { PriceDisplayComponent } from './price-display/price-display.component';
-import { ProductRowComponent } from './product-row/product-row.component';
-import { ProductsListComponent } from './products-list/products-list.component';
+import {AppComponent} from './app.component';
+import {ProductImageComponent} from './product-image/product-image.component';
+import {ProductDepartmentComponent} from './product-department/product-department.component';
+import {PriceDisplayComponent} from './price-display/price-display.component';
+import {ProductRowComponent} from './product-row/product-row.component';
+import {ProductsListComponent} from './products-list/products-list.component';
+import {ProductService} from "./product-service.service";
 
 @NgModule({
   declarations: [
@@ -18,13 +20,19 @@ import { ProductsListComponent } from './products-list/products-list.component';
     PriceDisplayComponent,
     ProductRowComponent,
     ProductsListComponent
+
   ],
   imports: [
+    LocalStorageModule.withConfig({
+      prefix: 'my-app',
+      storageType: 'localStorage'
+    }),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
